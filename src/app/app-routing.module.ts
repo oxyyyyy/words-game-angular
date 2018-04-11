3abf05a3d1f61124efe24c241e1e7946de709c7f
Alexandr Vlasenko <olexandr.vlasenko@gmail.com>
2018-04-11 10:57:31 +0300

    SidebarPlayersPanelComponent done!

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'
+import { CommonModule } from '@angular/common';
+
+import { SidebarPlayersPanelComponent } from './sidebar-players-panel/sidebar-players-panel.component'
+
+const routes: Routes = [
+  {
+    path: 'sidebar-players-panel/:3',
+    component: SidebarPlayersPanelComponent
+  }
+]
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
--- src/app/sidebar-players-panel/sidebar-players-panel.component.ts
+++ src/app/sidebar-players-panel/sidebar-players-panel.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Player } from '../players'
-import { team1Players } from '../mock-team'
-import { team2Players } from '../mock-team'
+import { ActivatedRoute } from '@angular/router';
+
+import { Player } from '../players';
+import { team1Players } from '../mock-team';
+import { team2Players } from '../mock-team';
 
 @Component({
   selector: 'app-sidebar-players-panel',
@@ -13,7 +15,9 @@ export class SidebarPlayersPanelComponent implements OnInit {
   team1Players = team1Players;
   team2Players = team2Players;
 
-  constructor() { }
+  // constructor(private route: ActivatedRoute) { 
+  //   this.route.params.subscribe(res => console.log(res.id))
+  //  }
 
   ngOnInit() {
   }
